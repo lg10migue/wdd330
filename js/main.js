3fb17fc59ef5a80eb5e2c7098b07f95e1c4c5e8c
lg10migue <lop18028@byui.edu>
2022-09-17 20:58:20 -0600

    Add Week 1 content.
    Add directory, pages and notes for week 1.

--- js/main.js
+++ js/main.js
@@ -2,28 +2,55 @@
 
 const links = [
     {
-        label: "Week 1",
-        url: ""
+        label : "Week 1",
+        contents : [
+            {
+                label : "Story Editor",
+                url : "week1/story_editor.html"
+            },
+            {
+                label : "Notes",
+                url : "week1/notes.html"
+            }
+        ]
     }
 ] ;
 
 createTableOfContents( links ) ;
 
 function createTableOfContents( links ) {
-    const ol = document.querySelector( "ol" ) ;
+    const main = document.querySelector( "main" ) ;
     links.forEach( link => {
 
-        // Create a element with attributes.
-        let a = document.createElement( "a" ) ;
-        a.innerHTML = link.label ;
-        a.href = link.url ;
-        a.target = "_blank" ;
+        // Create h2 element for label name.
+        let h2 = document.createElement( "h2" ) ;
+        h2.innerHTML = link.label ;
 
-        // Create li element and attach to a element.
-        let li = document.createElement( "li" ) ;
-        li.appendChild( a ) ;
+        // Attach h2 element to main.
+        main.appendChild( h2 ) ;
 
-        // Attach li element to ol element.
-        ol.appendChild( li ) ;
+        // Create list of subcontents.
+        const ol = document.createElement( "ol" ) ;
+
+        // Attach elements to table of subcontents.
+        link.contents.forEach( element => {
+
+            // Create a element with attributes.
+            let a = document.createElement( "a" ) ;
+            a.innerHTML = element.label ;
+            a.href = element.url ;
+            a.target = "_blank" ;
+
+            // Create li element and attach a element.
+            let li = document.createElement( "li" ) ;
+            li.appendChild( a ) ;
+
+            // Attach li element to ol element.
+            ol.appendChild( li ) ;
+
+            // Attach ol element to main.
+            main.appendChild( ol ) ;
+
+        } ) ;
     } ) ;
 } ;
